19d202da20a70e2315bc620ee715b064802a2bea
tenthyoung <tenthyoung@gmail.com>
2020-05-31 16:11:31 -0700

    this install is a huge failure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
+
+export default App;
